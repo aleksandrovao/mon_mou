13af3bfb485af139f4955f0e4ab0fdfab41364d7
Оксана Александрова <aleksandrovao767@gmail.com>
2021-12-02 16:35:19 +0200

    add slick

--- #src/js/main.js
+++ #src/js/main.js
@@ -4,3 +4,34 @@ $('.header__top-btn-menu').click(function(){
 $('.aside__top-btn-close').click(function(){
     $('.aside').removeClass('aside--close')
 })
+
+$(document).ready(function(){
+    $('.slick').slick({
+      mobileFirst: true,
+      variableWidth: true,
+      arrows: false,
+     responsive: [{
+         breakpoint: 768,
+         settings:{
+             slidesToShow: 2,
+             slidesToScroll: 3
+         }
+        },
+        {
+            breakpoint: 920,
+            settings:{
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                
+            }
+           },
+           {
+            breakpoint: 1200,
+            settings:{
+                
+                arrows: true,
+            }
+           },
+     ]
+  })
+});
--- #src/js/script.js
+++ #src/js/script.js
@@ -3,5 +3,5 @@
 @@include("main.js");
 
 // ("bootstrap.bundle.js");
-
+@@include("slick.js")
 // Чтобы подключить слайдер, сначала подключаем jquery и потом слик-слайдер
