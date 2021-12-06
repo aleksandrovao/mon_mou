e183bbd6bdccf60ee9f56794e9446ff4189ae83c
Оксана Александрова <aleksandrovao767@gmail.com>
2021-12-06 09:36:48 +0200

    add search and gallery

--- dist/js/script.js
+++ dist/js/script.js
@@ -10933,7 +10933,34 @@ $(document).ready(function(){
            },
      ]
   })
-});;
+});
+
+// search
+searchBtn = $('.header__top-search');
+search = $('.search');
+tel = $('.header__top-tel')
+header = $('.header__body')
+content = $('.header__content')
+$('active')
+
+searchBtn.click(function(){
+    search.addClass('active')
+    searchBtn.css({'display':'none'})
+    tel.css({'display':'none'})
+})
+header.click(function(){
+    search.removeClass('active')
+    setTimeout(function(){
+        searchBtn.css({'display':'block'})
+    }, 500)
+})
+content.click(function(){
+    search.removeClass('active')
+    setTimeout(function(){
+        searchBtn.css({'display':'block'})
+        tel.css({'display':'block'})
+    }, 500)
+});
 
 // ("bootstrap.bundle.js");
 /*
